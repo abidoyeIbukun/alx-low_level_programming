f9c59dac734151a49f411834ec2370843c32961f
abidoyeIbukun <abidoyeibukun@gmail.com>
2023-07-13 18:30:18 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stadio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void) {
+	puts("Programming is like building a multilingual puzzle\n");
+	return 0;
+
+}	
