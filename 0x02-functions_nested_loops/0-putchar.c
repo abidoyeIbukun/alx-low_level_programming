1ee5266a2937600c43e1d712bf9fc42ddf4c7afe
abidoyeIbukun <abidoyeibukun@gmail.com>
2023-07-19 07:10:53 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
